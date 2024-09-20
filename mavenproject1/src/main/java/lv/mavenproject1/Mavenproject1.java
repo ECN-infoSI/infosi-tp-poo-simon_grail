87e264c52bf7d436ad4e567c6321d45fd07d66fe
Erazal75 <lazare.grail@gmail.com>
2024-09-20 09:47:01 +0200

    Ca fonctionne!!

---
+++ mavenproject1/src/main/java/lv/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package lv.mavenproject1;
+
+/**
+ *
+ * @author lazaregrail
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
